a49339ee1b6e91f54d07db3fd6da408074cb7d11
ManarOmar <manar.aljawarneh@gmail.com>
2019-10-21 10:00:05 +0300

    update the way of importing utils file

--- app/run.py
+++ app/run.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('../')
+
 import json
 import plotly
 import pandas as pd
@@ -8,7 +11,7 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+#from sklearn.externals import joblib
 from sqlalchemy import create_engine
 from models.utils import Tokenizer, UrlExtractor, MessageLength
 
@@ -19,7 +22,7 @@ engine = create_engine('sqlite:///../data/disaster.db')
 df = pd.read_sql_table('messages', engine)
 
 # load model
-model = joblib.load("../models/clf.pkl")
+#model = joblib.load("../models/clf.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
