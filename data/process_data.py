6f83422f6d86b9c775c1044a9a5e456525a02b72
ManarOmar <manar.aljawarneh@gmail.com>
2019-10-21 08:13:15 +0300

    first commit

---
+++ data/process_data.py
@@ -0,0 +1,115 @@
+import sys
+import numpy as np
+import pandas as pd
+import json
+from sklearn.metrics import confusion_matrix, make_scorer
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
+from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
+
+def load_data(messages_filepath, categories_filepath):
+  """
+load the data from the csv files into dataframe
+
+INPUT :
+message_filepath -> csv file that handle the text messages
+categories_file_path -> csv file that handle the categories for the messages ids
+
+OUTPUT:
+df -> the merged dataframefrom messages and categories
+  """
+    #read the two files
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    #merge the two dataframes on id
+    df = pd.merge(messages, categories, on = 'id')
+
+    return df
+
+def clean_data(df):
+
+  """
+  cleaning the dataframe using these steps:
+  1. create a dataframe of the 36 individual category columns
+  2. extract a list of new column names for categories.
+  3.rename the columns of categories and convert the type of them to string
+  4.concatenate the original dataframe with the new categories dataframe
+  5.drop duplicates
+
+  INPUT -> uncleaned dataframe
+  OUTPUT -> cleaned dataframe
+  """
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(pat = ";", expand = True)
+    
+    # select the first row of the categories dataframe
+    row = categories.iloc[0, :]
+    # extract a list of new column names for categories.
+  
+    category_colnames = row.apply(lambda x:x.split('-')[0]).values.tolist()
+    
+    # rename the columns of categories
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x:x.split('-')[1])
+    
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+        
+    # drop the original categories column from `df`
+    df.drop('categories', axis=1, inplace=True)    
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories], axis=1)
+
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+
+    return df
+
+
+def save_data(df, database_filename):
+  """
+  saving the cleaned dataframe to a database file
+  INPUT:
+  df -> the cleaned dataframe
+  database_filename -> the name of the database file you want to save in
+  """
+    engine = create_engine('sqlite:///'+database)
+    df.to_sql(sqltable, engine, index=False)  
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/train_classifier.py
@@ -0,0 +1,167 @@
+# import libraries
+import nltk
+nltk.download(['punkt', 'averaged_perceptron_tagger', 'wordnet', 'stopwords'])
+import json
+import joblib
+import numpy as np
+import pandas as pd
+import string
+import re
+impoer sys
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from sklearn.metrics import confusion_matrix, make_scorer
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer,TfidfVectorizer
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sqlalchemy import create_engine
+from sklearn.ensemble import BaggingClassifier, RandomForestClassifier, AdaBoostClassifier
+from sklearn.multioutput import MultiOutputClassifier
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
+
+
+def load_data(database_filepath):
+    """
+    Load Data Function
+    
+    INPUT:
+        no argument 
+    Output:
+        X -> feature(text message) array values
+        Y -> label array values
+        
+    """
+    engine = create_engine('sqlite:///'+database_حشفا)
+    df = pd.read_sql_table("messages", con = engine)
+    X = df['message']
+    Y = df.iloc[:, 4:]
+    category_names = Y.columns.tolist()
+    return X,Y, categories
+    
+
+
+def tokenize(text):
+    pass
+
+
+def build_model():
+    """
+    building the model using machine learning pipelines 
+    and grid search to validate the model
+    """
+
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+
+            ('text_pipeline', Pipeline([
+                ('tokenize', Tokenizer()),
+                ('vect', CountVectorizer()),
+                ('tfidf', TfidfTransformer())
+            ])),
+
+            ('url_pipeline',Pipeline([
+                ('tokenize', Tokenizer()),
+                ('containing_url', UrlExtractor())
+                ])),
+            ('computing_message_length',MessageLength())       
+            
+        ])),
+
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    
+    parameters = {
+            'features__text_pipeline__vect__ngram_range': ((1, 1), (1, 2)),
+            'features__text_pipeline__vect__max_df': (0.5, 0.75, 1.0),
+            'features__text_pipeline__vect__max_features': (None, 5000, 10000),
+            'clf__estimator__n_estimators': [50, 100,200] ,
+            'features__transformer_weights': (
+                {'text_pipeline': 1, 'url_pipeline' : 0.5, 'computing_message_length' : 0.5}, 
+                {'text_pipeline': 0.5, 'url_pipeline' : 1,  'computing_message_length' : 1},
+                {'text_pipeline': 0.8, 'url_pipeline' : 1,  'computing_message_length' : 1})
+                 
+            
+        
+        }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    return cv
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    """
+    evaluate the model performance using accuracy, precision, f1 score
+    Arguments:
+        model -> the model which is built using the build_model function
+        X_test -> the messages array in the testing dataset
+        Y_test -> the labels array for the messages 
+    Output:
+        print the evaluation metrices  and the parameters for the best model specified by grid search 
+
+    """
+
+    y_pred = model.predict(X_test)
+
+    for i, cat in enumerate(category_names):
+
+        labels = np.unique(y_pred)
+        confusion_mat = confusion_matrix(Y_test.iloc[:,i], y_pred[:,i], labels=labels)
+        accuracy = (y_pred[:,i] == Y_test.iloc[:,i]).mean()
+        class_report = classification_report(Y_test.iloc[:,i], y_pred[:,i])
+        f1_sc = f1_score(Y_test.iloc[:,i], y_pred[:,i])
+        prec = precision_score(Y_test.iloc[:,i], y_pred[:,i])
+
+        print("Labels:", labels)
+        print("Confusion Matrix:\n", confusion_mat)
+        print("Accuracy: ", accuracy)
+        print("precision: ", prec)
+        print("f1_score: ", f1_sc)
+        print("\nClassification report:\n ", class_report ) 
+
+    print("\nBest Parameters: ", model.best_params_)
+
+def save_model(model, model_filepath):
+   """dumps the model to the given filepath
+    Args:
+        model -> the model which is built using the build_model function
+        model_filepath (string): the filepath to save the model to
+    Returns:
+        None
+    """
+    joblib.dump(model, model_filepath)
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
